275c29e0ca4910d07bd9111649e867178e998df0
Brijendra <bps.brijendra@gmail.com>
2018-05-22 23:06:52 +0530

    First Commit

---
+++ src/test/java/page/GooglePage.java
@@ -0,0 +1,11 @@
+package page;
+
+import utils.GenericUtil;
+
+public class GooglePage  extends GenericUtil{
+	
+	public void testForPageClass(){
+		System.out.println("Test Page object complete");
+	}
+
+}
